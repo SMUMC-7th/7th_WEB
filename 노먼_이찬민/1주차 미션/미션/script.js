b7e85b01ed31e4a7beb19ad736ea4c6309084e3d
ChanMinLee2 <dlcksals87@gmail.com>
2024-09-23 00:31:09 +0900

    🎨 FEAT (week1) : 1주차 미션 todolist 스켈레톤

---
+++ 노먼_이찬민/1주차 미션/미션/script.js
@@ -0,0 +1,48 @@
+const todoList = document.getElementById("TodoList");
+const completeList = document.getElementById("CompleteList");
+const todoForm = document.getElementById("TodoForm");
+const todoInput = document.getElementById("TodoInput");
+
+todoForm.addEventListener("submit", addTodoItem);
+
+function addTodoItem(e) {
+  e.preventDefault();
+  const todoText = todoInput.value.trim();
+  if (todoText === "") {
+    alert("내용을 입력하세요!");
+    return;
+  }
+
+  const todoItem = document.createElement("li");
+  todoItem.classList.add("TodoItem");
+  todoItem.textContent = todoText;
+
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "완료";
+  completeButton.addEventListener("click", completeTodoItem);
+
+  todoItem.appendChild(completeButton);
+  todoList.appendChild(todoItem);
+}
+
+function completeTodoItem(e) {
+  const todoItem = e.target.parentElement;
+  // console.log(todoItem.children[0]);
+  // 기존 버튼 삭제
+  todoItem.children[0].remove();
+
+  // 새 버튼 붙이기
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "삭제";
+  deleteButton.addEventListener("click", removeTodoItem);
+  todoItem.appendChild(deleteButton);
+
+  // 위치 이동
+  completeList.appendChild(todoItem);
+  todoList.remove(todoItem);
+}
+
+function removeTodoItem(e) {
+  const todoItem = e.target.parentElement;
+  todoItem.remove();
+}
