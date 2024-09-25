5a8f43ad0c75c6569d45449c8fe6674e5bda4f36
jaeyoungmun <mjy0694@naver.com>
2024-09-25 18:36:48 +0900

    FEAT: 스크롤+텍스트줄임 기능추가

--- 제리_문재영/1주차 미션/to_do_list.js
+++ 제리_문재영/1주차 미션/to_do_list.js
@@ -1,25 +1,52 @@
 
-function add_task(){
+function addTask(){ 
     // console.log(window.event)
-    if (window.event.keyCode == 13){
-        // const newTask = input.value.trim();
-        
-        const newDiv = document.createElement('div');
+    if (enterCheck()){ //엔터키 입력시
         const input = document.querySelector('input');
-        newDiv.className = 'list ing_task';
-        newDiv.textContent = input.value;
+        // console.log(input.value);
         
-        const newBtn = document.createElement('button');
-        newBtn.textContent = '완료'
-        newDiv.appendChild(newBtn);
-        newBtn.addEventListener('click', function(){
-            tasked(newDiv, newBtn);
-        });
-        
-        console.log(newDiv)
-        const ing_list_box = document.querySelector('.ing_list_box');
-        ing_list_box.appendChild(newDiv);
-        input.value = ''
+        if (inputCheck(input.value)){ //빈칸인지 체크
+            //해야할일 생성
+            const newDiv = document.createElement('div');
+            newDiv.className = 'list ing_task';
+            const textInput = document.createElement('div');
+            textInput.className = 'textInput';
+            // textInput.setAttribute('contenteditable','true');
+            newDiv.appendChild(textInput);
+            textInput.textContent = input.value;
+
+            const newBtn = document.createElement('button');
+            newBtn.textContent = '완료'
+            newDiv.appendChild(newBtn);
+            newBtn.addEventListener('click', function(){ //완료시 해낸일로 이동
+                tasked(newDiv, newBtn);
+            });
+
+            //스크롤 하단고정 !해낸일 스크롤은 하단고정이 안 됩니다ㅠ
+            const listScroll1 = document.querySelector('.ing_list_box');
+            listScroll1.scrollTop = listScroll1.scrollHeight;
+            const listScroll2 = document.querySelector('.ed_list_box');
+            listScroll2.scrollTop = listScroll2.scrollHeight;
+
+            //텍스트에 커서 올리면 커서바뀌면서 클릭시에 텍스트 수정가능
+            textInput.addEventListener('click', function(){
+                // textInput.value를 받아두고 숨기기
+                // input태그로 바꾸고 value값 넣어두기
+                console.log(textInput);
+                // newBtn.style.display = 'hide';
+                // if (enterCheck){
+                //     newBtn.style.display = 'show';
+                // }
+            });
+            // input.focus();
+            // console.log(newDiv)
+            const ing_list_box = document.querySelector('.ing_list_box');
+            ing_list_box.appendChild(newDiv);
+            input.value = ''
+        }
+        else{
+            alert('내용을 입력하세요');
+        }
     }
 }
 
@@ -34,3 +61,17 @@ function tasked(newDiv, newBtn){
         newDiv.remove();
     })
 }
+
+function inputCheck(input){
+    if (input == ''){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function enterCheck(){
+    if (window.event.keyCode == 13) return true;
+    else return false;
+}
+//
