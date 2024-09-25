d5f7ba4370c39b29799072a149ecbf437f3a55e0
202110861 <dbsalsk2514@naver.com>
2024-09-26 00:41:19 +0900

    스터디 미션4(추가미션1) 리스트 추가할때마다 바로 보이는거 실패..

--- 라분_윤미나/example.js
+++ 라분_윤미나/example.js
@@ -16,16 +16,22 @@ function addTodoItem(){
     //리스트 생성
     const listItem=document.createElement('p');
     listItem.className="listItem";
-    listItem.innerText=todoText;
 
+    //텍스트를 감싸는 span태그 생성
+    const textSpan = document.createElement('span');
+    textSpan.innerText=todoText;
+
+    //버튼 생성
     const button=document.createElement('button');
     button.className="complete";
     button.innerText='완료';
     button.addEventListener("click",  function(){markAsDone(listItem, button);});
 
     //리스트에 추가
-    todoList.appendChild(listItem);
+    listItem.appendChild(textSpan);
     listItem.appendChild(button);
+    todoList.appendChild(listItem);
+
     todoInput.value='';
 }
 
