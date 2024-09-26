1997e783c3d900f7696e314ebc3a33cc7ede9d45
202110861 <dbsalsk2514@naver.com>
2024-09-26 09:14:16 +0900

    스터디 미션4(추가미션 1,2 완료)

--- 라분_윤미나/example.js
+++ 라분_윤미나/example.js
@@ -17,9 +17,12 @@ function addTodoItem(){
     const listItem=document.createElement('p');
     listItem.className="listItem";
 
-    //텍스트를 감싸는 span태그 생성
+    //span태그 생성
     const textSpan = document.createElement('span');
     textSpan.innerText=todoText;
+
+    //텍스트 클릭시 수정
+    textSpan.addEventListener("click", function(){editTodoList(listItem, textSpan,button);});
 
     //버튼 생성
     const button=document.createElement('button');
@@ -32,6 +35,8 @@ function addTodoItem(){
     listItem.appendChild(button);
     todoList.appendChild(listItem);
 
+    todoList.scrollTop = todoList.scrollHeight;
+
     todoInput.value='';
 }
 
@@ -40,15 +45,60 @@ function markAsDone(listItem,button){
     const doneList = document.getElementById('done-list');
 
     button.innerText='삭제';
-    button.addEventListener("click",  deleteItem);
     
-    todoItem.innerText=todoItem.firstChild.textContent;
+    const textSpan = listItem.querySelector('span');
+    textSpan.innerText=todoItem.firstChild.textContent;
+    
     button.addEventListener("click",  function(){deleteItem(listItem);});
         
-    doneList.appendChild(todoItem);
-    todoItem.appendChild(button);
+    
+    listItem.appendChild(textSpan);
+    listItem.appendChild(button);
+    doneList.appendChild(listItem);
+
+    doneList.scrollTop = doneList.scrollHeight;
+    
 }
 
 function deleteItem(listItem){
     listItem.remove();
+}
+
+function editTodoList(listItem, textSpan,button){
+    
+    button.style.display='none'; //버튼 안보이게 함.
+    const text=textSpan.innerText; //텍스트 가져오기
+    const inputItem=document.createElement('input'); //새로운 input 생성
+
+    //inputItem.type='text';
+    inputItem.value=text;
+    inputItem.className='edit-input';
+
+    inputItem.addEventListener('keypress',function(e){
+        
+        if(e.key==='Enter'){
+            if(inputItem.value===''){
+                alert('할 일을 입력해 주세요.');
+            }
+            else saveTodoLIst(listItem, inputItem,button);
+        }
+    });
+    
+    //기존 텍스트를 input으로 대체
+    listItem.replaceChild(inputItem,textSpan);
+
+}
+
+function saveTodoLIst(listItem, inputItem,button){
+    const newText=inputItem.value.trim();
+
+    const newTextSpan = document.createElement('span');
+    newTextSpan.innerText=newText;
+
+    newTextSpan.addEventListener('click',function(){
+        editTodoList(listItem, textSpan,button);
+    });
+
+    listItem.replaceChild(newTextSpan,inputItem);
+    button.style.display='inline';
 }
