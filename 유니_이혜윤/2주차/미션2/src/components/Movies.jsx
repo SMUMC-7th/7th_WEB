d20b66106fff6936ad3b997434c09cf232662d1a
hyeyoon23 <51lhy@naver.com>
2024-09-30 10:22:37 +0900

    FEAT: 미션2 영화컴포넌트 마우스호버

--- 유니_이혜윤/2주차/미션2/src/components/Movies.jsx
+++ 유니_이혜윤/2주차/미션2/src/components/Movies.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import { MOVIES } from "../mocks/movies";
 
 const Movies = () => {
-    console.log(MOVIES);
+    // console.log(MOVIES);
+
+    const [hoverMovie, setHoverMovie] = useState(null);
+
+    const handleMouseEnter = (movieId) => {
+        setHoverMovie(movieId);
+    };
+
+    const handleMouseLeave = () => {
+        setHoverMovie(null);
+    };
+
 
     return (
         <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px', backgroundColor: '#060f1c'}}>
             {MOVIES.results.map((movie) => (
-                <div key={movie.id} style={{marginTop: '50px'}}>
+                <div key={movie.id} style={{marginTop: '50px', position: 'relative'}}
+                    onMouseEnter = {() => handleMouseEnter(movie.id)}
+                    onMouseLeave = {handleMouseLeave}>
                     <img
                         src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
                         alt='사진'
                         style={{borderRadius: '5px'}}
                     />
+                    {hoverMovie === movie.id && (
+                        <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',
+                            backgroundColor: 'rgba(0, 0, 0, 0.7)'
+                        }}>
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
