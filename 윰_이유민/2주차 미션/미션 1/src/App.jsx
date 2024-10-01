9246bfed67a7aa214aaed8adba1482ab3663bb21
Whatdoyumin <lym4963@naver.com>
2024-10-02 00:01:48 +0900

    ✨ feat: 2주차 미션 1 컴포넌트 구분 (Input, Button)

--- 윰_이유민/2주차 미션/미션 1/src/App.jsx
+++ 윰_이유민/2주차 미션/미션 1/src/App.jsx
@@ -1,53 +1,40 @@
 import { useState } from "react";
 import "./todo.css";
-
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
-  // 투두 리스트, 화면에 출력되는 (추가, 삭제, 수정)
-  const [todos, setTodos] = useState([
-    
-  ]);
-
+  const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
-
   const [editingId, setEditingId] = useState('');
   const [editText, setEditText] = useState('');
 
-  // 렌더링 방지
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
-  // 1. 추가하기
-  
   const addTodo = () => {
-    if (text.trim().length === 0){
+    if (text.trim().length === 0) {
       alert('텍스트를 입력하세요!');
     } else {
       setTodos((prev) => [
         ...prev,
-        {id: Math.floor(Math.random() * 100) + 2, task: text }
+        { id: Math.floor(Math.random() * 100) + 2, task: text },
       ]);
       setText('');
     }
-    
   };
-
-  // 2. 삭제하기
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((item) => item.id !== id
-    ));
+    setTodos((prev) => prev.filter((item) => item.id !== id));
   };
 
-  // 3. 수정하기 (핵심)
   const updateTodo = (id, text) => {
-    setTodos((prev) => 
-      prev.map((item) => item.id === id ? {...item, task: text} : item)
+    setTodos((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, task: text } : item))
     );
     setEditingId('');
   };
-
 
   return (
     <div className="container">
@@ -55,28 +42,30 @@ function App() {
         <h1>ToDoList</h1>
       </header>
       <form className="submitForm" onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-        <button onClick={() => addTodo()} type="submit">할 일 등록</button>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+        <Button onClick={addTodo} type="submit" text="할 일 등록" />
       </form>
       <div className="todoList">
-        {todos.map((todo) => (
+        {todos.map((todo, index) => (
           <div key={todo.id} className="todos">
             <div className="todo">
-              <p className="todo__id">{todo.id}번</p>
-              { editingId != todo.id ? (
+              <p className="todo__id">{index + 1}번</p>
+              {editingId !== todo.id ? (
                 <p>{todo.task}</p>
               ) : (
-                <input type="text" defaultValue={todo.task} onChange={(e) => setEditText(e.target.value)} />
-              ) }
+                <Input
+                  defaultValue={todo.task}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+              )}
             </div>
 
             <div className="btnContainer">
-              <button id="delete" onClick={() => deleteTodo(todo.id)}>삭제하기</button>
-              
+              <Button id="delete" onClick={() => deleteTodo(todo.id)} text="삭제하기" />
               {editingId === todo.id ? (
-                <button id="update" onClick={() => updateTodo(editingId, editText)}>수정 완료</button>
+                <Button id="update" onClick={() => updateTodo(editingId, editText)} text="수정 완료" />
               ) : (
-                <button id="update" onClick={() => setEditingId(todo.id)}>수정 진행</button>
+                <Button id="update" onClick={() => setEditingId(todo.id)} text="수정 진행" />
               )}
             </div>
           </div>
