d3eb20b6f9d668e23280743eeb722acf76c53f91
Whatdoyumin <lym4963@naver.com>
2024-10-10 16:17:16 +0900

    💄 style: 라우터의 각 컴포넌트 index.js에서 관리하도록 변경

--- 윰_이유민/3주차 미션/미션 1/src/App.jsx
+++ 윰_이유민/3주차 미션/미션 1/src/App.jsx
@@ -1,65 +1,65 @@
-import './App.css'
+import './App.css';
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-
-import HomePage from "./pages/home/Home.jsx";
-import NotFound from "./pages/not-found.jsx";
-import MovieList from "./pages/movieList/MovieList.jsx";
-import RootLayout from "./layout/root-layout.jsx";
-import Login from './pages/login/Login.jsx';
-import Signup from './pages/signup/Signup.jsx';
-import Search from './pages/search/Search.jsx';
-import MovieCategory from './pages/movieCategory/MovieCategory.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  RootLayout,
+  NotFound,
+  HomePage,
+  Search,
+  MovieCategory,
+  MovieList,
+  Login,
+  Signup,
+} from './pages/index';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout/>,
-        errorElement: <NotFound/>,
-        children: [
-            {
-                index: true,
-                element: <HomePage/>
-            },
-            {
-                path: '/search',
-                element: <Search />
-            },
-            {
-                path: '/movies',
-                element: <MovieCategory/>
-            },
-            {
-                path: '/movies/now-playing',
-                element: <MovieList category="now_playing" />
-            },
-            {
-                path: '/movies/popular',
-                element: <MovieList category="popular" />
-            },
-            {
-                path: '/movies/top-rated',
-                element: <MovieList category="top_rated" />
-            },
-            {
-                path: '/movies/up-coming',
-                element: <MovieList category="upcoming" />
-            },
-            {
-                path: '/login',
-                element: <Login />
-            },
-            {
-                path: '/signup',
-                element: <Signup />
-            }
-        ]
-    },
-
-])
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/search',
+        element: <Search />,
+      },
+      {
+        path: '/movies',
+        element: <MovieCategory />,
+      },
+      {
+        path: '/movies/now-playing',
+        element: <MovieList category="now_playing" />,
+      },
+      {
+        path: '/movies/popular',
+        element: <MovieList category="popular" />,
+      },
+      {
+        path: '/movies/top-rated',
+        element: <MovieList category="top_rated" />,
+      },
+      {
+        path: '/movies/up-coming',
+        element: <MovieList category="upcoming" />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/signup',
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 function App() {
-    return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
