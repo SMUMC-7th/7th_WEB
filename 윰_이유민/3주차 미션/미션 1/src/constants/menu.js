1c17589c17e71e616c612bfc04f6137f235cd5b0
Whatdoyumin <lym4963@naver.com>
2024-10-10 17:14:58 +0900

    ♻ refactor: 카테고리 상수 배열 사용으로 코드 간결화

---
+++ 윰_이유민/3주차 미션/미션 1/src/constants/menu.js
@@ -0,0 +1,28 @@
+const CATEGORY = [
+  {
+    id: 1,
+    text: "현재 상영 중인",
+    img_path: "https://cdn.pixabay.com/photo/2024/02/12/17/49/theater-8569119_1280.jpg",
+    url_path: "/movies/now-playing"
+  },
+  {
+    id: 2,
+    text: "인기 있는",
+    img_path: "https://cdn.pixabay.com/photo/2016/06/22/11/10/box-1472804_1280.png",
+    url_path: "/movies/popular"
+  },
+  {
+    id: 3,
+    text: "높은 평가를 받은",
+    img_path: "https://cdn.pixabay.com/photo/2015/05/06/03/56/blue-754864_1280.png",
+    url_path: "/movies/top-rated"
+  },
+  {
+    id: 4,
+    text: "개봉 예정 중인",
+    img_path: "https://cdn.pixabay.com/photo/2015/08/13/23/47/background-image-887729_1280.jpg",
+    url_path: "/movies/up-coming"
+  }
+]
+
+export { CATEGORY }
