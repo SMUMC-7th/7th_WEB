921f5456f16049ccc8c7d3c4a2d309bbe7559571
Whatdoyumin <lym4963@naver.com>
2024-10-12 22:27:34 +0900

    ✨ feat: 영화 카드 prettier 적용

--- 윰_이유민/3주차 미션/미션 1/src/components/movieCard/MovieCard.jsx
+++ 윰_이유민/3주차 미션/미션 1/src/components/movieCard/MovieCard.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
-import * as S from './MovieCard.style.js'
+import * as S from './MovieCard.style.js';
 
 function MovieCard(movie) {
-
   const [isHover, setIsHover] = useState(false);
-  const {  poster_path, title, release_date} = movie.movie;
+  const { poster_path, title, release_date } = movie.movie;
 
-  return <S.Container>
-    <S.ImageContainer>
-      <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" onMouseOver={() => setIsHover(true)} onMouseOut={() => {setIsHover(false)}} />
-      { isHover && <S.Backdrop />}
-    </S.ImageContainer>
-    <S.InfoContainer>
-      <h5>{title}</h5>
-      <p>{release_date}</p>
-    </S.InfoContainer>
-  </S.Container>
+  return (
+    <S.Container>
+      <S.ImageContainer>
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+          alt=""
+          onMouseOver={() => setIsHover(true)}
+          onMouseOut={() => {
+            setIsHover(false);
+          }}
+        />
+        {isHover && <S.Backdrop />}
+      </S.ImageContainer>
+      <S.InfoContainer>
+        <h5>{title}</h5>
+        <p>{release_date}</p>
+      </S.InfoContainer>
+    </S.Container>
+  );
 }
 
 export default MovieCard;
