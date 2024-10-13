d295c2e67242dfebfeac08e1810c2a06e7b85379
Whatdoyumin <lym4963@naver.com>
2024-10-14 01:13:34 +0900

    💄 style: Movies 목록 grid 크기 변경

--- 윰_이유민/4주차 미션/추가 미션 1/src/components/movieList/MovieList.style.js
+++ 윰_이유민/4주차 미션/추가 미션 1/src/components/movieList/MovieList.style.js
@@ -12,9 +12,12 @@ const MovieList = styled.div`
   width: 100%;
   height: 400px;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
-  grid-template-rows: repeat(auto-fit, minmax(130px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(90px, 1fr));
+  grid-template-rows: 150px;
+  justify-content: center;
   gap: 20px;
+  row-gap: 20px;
+  padding: 0 20px;
 `
 
 export { Container, MovieList }
