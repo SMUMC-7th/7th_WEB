b1baaf704706a29c608169d60478f66ee6f8467a
김연진 <kyj0719@gmail.com>
2024-10-13 21:55:09 +0900

    FIX: movieList 스타일링 수정

--- 코튼_김연진/4주차 미션/추가 미션 1/src/components/movieList/movieList.style.js
+++ 코튼_김연진/4주차 미션/추가 미션 1/src/components/movieList/movieList.style.js
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     gap: 10px;
+    justify-content: center;
+    margin: 10px 30px;
 `;
 
 export { Container };
