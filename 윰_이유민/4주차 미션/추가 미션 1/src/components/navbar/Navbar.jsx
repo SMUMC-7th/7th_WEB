9f5bd15871e35fa471c2513462469e06a6144484
Whatdoyumin <lym4963@naver.com>
2024-10-14 00:58:05 +0900

    💄 style: react-icons 로고 사용

--- 윰_이유민/4주차 미션/추가 미션 1/src/components/navbar/Navbar.jsx
+++ 윰_이유민/4주차 미션/추가 미션 1/src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import * as S from "./Navbar.style"
+import { FaBasketShopping } from "react-icons/fa6";
+import * as S from "./Navbar.style";
 
 function navBar() {
+  
   return (
     <S.Container>
-      <h3>영화 장바구니 망둥기</h3>
+      <S.Logo>
+        <FaBasketShopping />
+        <h3>영화 장바구니 망둥기</h3>
+      </S.Logo>
       <S.BtnContainer>
         <button onClick={() => handleGetToken()}>token</button>
         <button>session</button>
--- 윰_이유민/4주차 미션/추가 미션 1/src/components/navbar/Navbar.style.js
+++ 윰_이유민/4주차 미션/추가 미션 1/src/components/navbar/Navbar.style.js
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 50px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
   top: 0;
+`
+
+const Logo = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
 `
 
 const BtnContainer = styled.div`
@@ -28,4 +39,4 @@ const BtnContainer = styled.div`
   }
 `
 
-export { Container, BtnContainer }
+export { Container, Logo, BtnContainer }
