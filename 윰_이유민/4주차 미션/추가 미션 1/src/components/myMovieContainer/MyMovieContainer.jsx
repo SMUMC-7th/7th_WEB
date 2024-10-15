770b2ce55c6f1b020429392ab248419664cd1888
Whatdoyumin <lym4963@naver.com>
2024-10-15 23:22:37 +0900

    ✨ feat: 영화 리스트 상태 관리 적용 및 영화 카드 삭제 기능 구현

--- 윰_이유민/4주차 미션/추가 미션 1/src/components/myMovieContainer/MyMovieContainer.jsx
+++ 윰_이유민/4주차 미션/추가 미션 1/src/components/myMovieContainer/MyMovieContainer.jsx
@@ -1,18 +1,26 @@
 import useFetchData from "../../hooks/useFetchData.js";
+import { useRequestApi } from "../../hooks/useRequestApi.js";
 import { CreateListBtn } from "../buttons/CreateListBtn.jsx"
 import { DeleteBtn } from "../buttons/DeleteBtn.jsx"
 import { GetListBtn } from "../buttons/GetListBtn.jsx"
 import { MovieCard } from "../movieCard/MovieCard.jsx";
 import * as S from "./MyMovieContainer.style.js"
 
-function MyMovieContainer({ myMovies }) {
-  const list_id = localStorage.getItem('list_id');
+function MyMovieContainer({ myMovies, setMyMovies }) {
+  const { handleRequest, isLoading, isError } = useRequestApi();
 
-  const {
-    data: list,
-    isLoading,
-    isError,
-  } = useFetchData( list_id ? `/list/${list_id}?language=ko` : null);
+  const handleDeleteMovie = (movieId) => {
+    handleRequest({
+      method: "post",
+      url: `/list/${localStorage.getItem("list_id")}/remove_item?session_id=${localStorage.getItem("session_id")}`,
+      body: { media_id: movieId },
+      localKeys: ["list_id", "session_id"],
+      onSuccess: () => {
+        setMyMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== movieId));
+      },
+      errorMessage: "영화를 삭제하는 데 실패했습니다."
+    });
+  };
 
   return (
     <S.MyContainer>
@@ -22,23 +30,15 @@ function MyMovieContainer({ myMovies }) {
         <GetListBtn />
         <DeleteBtn />
       </S.BtnContainer>
-      {!list_id ? (
-        <p>장바구니를 생성해보세요!</p>
-      ) : isLoading ? (
-        <div>로딩중!</div>
-      ) : isError ? (
-        <div>ERROR</div>
-      ) : (
-        <S.MovieList>
-          {list?.data?.items?.length > 0 ? (
-            list.data.items.map((movie) => (
-              <MovieCard key={movie.id} {...movie} />
-            ))
-          ) : (
-            <></>
-          )}
-        </S.MovieList>
-      )}
+      <S.MovieList>
+        {myMovies.length > 0 ? (
+          myMovies.map((movie) => (
+            <MovieCard key={movie.id} {...movie} onClickMethod={() => handleDeleteMovie(movie.id)} />
+          ))
+        ) : (
+          <p>영화를 추가해보세요!</p>
+        )}
+      </S.MovieList>
     </S.MyContainer>
   )
 }
