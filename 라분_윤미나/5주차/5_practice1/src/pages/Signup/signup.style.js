a505febe647572f8f6896ccb7e50f8a7c48dd4e2
202110861 <dbsalsk2514@naver.com>
2024-10-27 00:21:02 +0900

    5주차 미션

--- 라분_윤미나/5주차/5_practice1/src/pages/Signup/signup.style.js
+++ 라분_윤미나/5주차/5_practice1/src/pages/Signup/signup.style.js
@@ -3,4 +3,34 @@ import styled from "styled-components";
 const H3 = styled.h3`
   color: white;
 `;
-export { H3 };
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    input {
+      width: 400px;
+      height: 40px;
+      background: white;
+      border: none;
+      border-radius: 10px;
+    }
+    p {
+      color: red;
+      margin: 0px;
+      font-size: 12px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background-color: rgb(255, 0, 119);
+  color: white;
+`;
+export { H3, Container, Button };
