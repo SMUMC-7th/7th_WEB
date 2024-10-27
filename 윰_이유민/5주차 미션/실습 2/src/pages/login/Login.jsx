f4f435e83429ff7d1d1dbb668ab8740405b7a77a
Whatdoyumin <lym4963@naver.com>
2024-10-27 16:52:32 +0900

    ✨feat: 로그인 페이지 useForm hook을 통한 유효성 검사 구현 (ver2)

--- 윰_이유민/5주차 미션/실습 2/src/pages/login/Login.jsx
+++ 윰_이유민/5주차 미션/실습 2/src/pages/login/Login.jsx
@@ -1,61 +1,56 @@
+import useForm from '../../hooks/useForm';
+import { validateLogin } from '../../utils/validate';
 import * as S from './Login.style';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
 
 const Login = () => {
-  const schema = yup.object().shape({
-    email: yup
-      .string()
-      .email('올바른 이메일 형식이 아닙니다. 다시 확인해주세요!')
-      .required('이메일을  입력해주세요.'),
-    password: yup
-      .string()
-      .min(8, '비밀번호는 8 ~ 16자 이내로 입력해주세요.')
-      .max(16, '비밀번호는 8 ~ 16자 이내로 입력해주세요.')
-      .required(),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { isSubmitting, errors, isValid },
-  } = useForm({
-    mode: 'onChange',
-    resolver: yupResolver(schema),
-    defaultValues: {
+  const login = useForm({
+    initialValue: {
       email: '',
       password: '',
     },
+    validate: validateLogin,
   });
 
-  const onSubmit = (data) => {
+  const { values, errors, touched, getTestInputProps } = login;
+
+  const handlePressLogin = () => {
+    console.log(values.email, values.password);
     alert('로그인 성공!');
-    console.log(data);
-    reset({});
   };
 
   return (
-    <S.Form noValidate onSubmit={handleSubmit(onSubmit)}>
+    <S.Form noValidate>
       <h2>로그인</h2>
       <S.Section>
         <S.Input
           type="email"
-          {...register('email')}
           placeholder="이메일을 입력해주세요!"
+          {...getTestInputProps('email')}
+          error={touched.email && errors.email}
         />
-        <S.ErrorMsg>{errors.email?.message}</S.ErrorMsg>
+        {touched.email && errors.email && (
+          <S.ErrorMsg>{errors.email}</S.ErrorMsg>
+        )}
       </S.Section>
       <S.Section>
         <S.Input
           type="password"
-          {...register('password')}
           placeholder="비밀번호를 입력해주세요!"
+          {...getTestInputProps('password')}
+          error={touched.password && errors.password}
         />
-        <S.ErrorMsg>{errors.password?.message}</S.ErrorMsg>
+        {touched.password && errors.password && (
+          <S.ErrorMsg>{errors.password}</S.ErrorMsg>
+        )}
       </S.Section>
-      <S.LoginBtn type="submit" disabled={!isValid || isSubmitting}>
+      <S.LoginBtn
+        type="submit"
+        onClick={handlePressLogin}
+        disabled={
+          (touched.email && errors.email) ||
+          (touched.password && errors.password)
+        }
+      >
         로그인
       </S.LoginBtn>
     </S.Form>
