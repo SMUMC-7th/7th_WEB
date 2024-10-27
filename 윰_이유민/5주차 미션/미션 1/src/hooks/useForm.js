88ba2c6a4c9f9d8df0151f3a809ef9a3a34902c2
Whatdoyumin <lym4963@naver.com>
2024-10-27 16:57:50 +0900

    🌱chore: 5주차 미션1 개발 환경 세팅

---
+++ 윰_이유민/5주차 미션/미션 1/src/hooks/useForm.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+function useForm({ initialValue, validate }) {
+  const [values, setValues] = useState(initialValue);
+
+  const [touched, setTouched] = useState({});
+
+  const [errors, setErrors] = useState({});
+
+  const handleChangeInput = (name, value) => {
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleBlur = (name) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
+  const getTestInputProps = (name) => {
+    const value = values[name];
+    const onChange = (event) => handleChangeInput(name, event.target.value);
+    const onBlur = () => handleBlur(name);
+
+    return { value, onChange, onBlur };
+  };
+
+  useEffect(() => {
+    const newErrors = validate(values);
+    console.log(newErrors);
+
+    setErrors(newErrors);
+  }, [validate, values]);
+
+  return { values, errors, touched, getTestInputProps };
+}
+
+export default useForm;
---
+++ 윰_이유민/5주차 미션/미션 1/src/pages/movieDetail/MovieDetail.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: transparent;
+  margin: 0 20px 40px;
+`
+export {Container}
