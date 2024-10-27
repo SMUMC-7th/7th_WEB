c4c5f850b39ca1d80895c4fe67965bfdc84a60fa
Whatdoyumin <lym4963@naver.com>
2024-10-27 21:58:48 +0900

    ✨feat: 회원가입 생년월일, 성별 입력 기능 추가

--- 윰_이유민/5주차 미션/미션 1/src/pages/signup/Signup.jsx
+++ 윰_이유민/5주차 미션/미션 1/src/pages/signup/Signup.jsx
@@ -1,3 +1,4 @@
+import { SIGNUP_FORM, USERSEX } from '../../constants/menu.js';
 import useForm from '../../hooks/useForm.js';
 import { validateSignup } from '../../utils/validate.js';
 import * as S from './Signup.style.js';
@@ -8,62 +9,72 @@ const Signup = () => {
       email: '',
       password: '',
       passwordCheck: '',
+      birthDate: '',
+      userSex: '',
     },
     validate: validateSignup,
   });
 
   const { values, errors, touched, getTestInputProps } = signup;
 
+  const isFormValid = () => {
+    return (
+      !errors.email &&
+      !errors.password &&
+      !errors.passwordCheck &&
+      !errors.userSex
+    );
+  };
+
   const handlePressSignup = () => {
     alert('회원가입 성공!');
-    console.log(values.email, values.password, values.passwordCheck);
+    console.log(
+      values.email,
+      values.password,
+      values.passwordCheck,
+      values.birthDate,
+      values.userSex
+    );
   };
 
   return (
-    <S.Form noValidate>
+    <S.Form noValidate onSubmit={handlePressSignup}>
       <h2>회원가입</h2>
+      {SIGNUP_FORM.map((field) => (
+        <S.Section key={field.id}>
+          <S.Title>{field.title}</S.Title>
+          <S.Input
+            type={field.type}
+            placeholder={field.placeholder}
+            {...getTestInputProps(field.name)}
+            error={touched[field.name] && errors[field.name]}
+          />
+          {touched[field.name] && errors[field.name] && (
+            <S.ErrorMsg>{errors[field.name]}</S.ErrorMsg>
+          )}
+        </S.Section>
+      ))}
       <S.Section>
-        <S.Input
-          type="email"
-          placeholder="이메일을 입력해주세요!"
-          {...getTestInputProps('email')}
-          error={touched.email && errors.email}
-        />
-        {touched.email && errors.email && (
-          <S.ErrorMsg>{errors.email}</S.ErrorMsg>
+        <S.Title>성별</S.Title>
+        <S.RadioContainer>
+          {USERSEX.map((option) => (
+            <S.Radio key={option.id}>
+              <input
+                type="radio"
+                {...getTestInputProps('userSex')}
+                id={option.id}
+                value={option.value}
+                name="userSex"
+              />
+              <S.Label htmlFor={option.id}>{option.label}</S.Label>
+            </S.Radio>
+          ))}
+        </S.RadioContainer>
+        {touched.userSex && errors.userSex && (
+          <S.ErrorMsg>{errors.userSex}</S.ErrorMsg>
         )}
       </S.Section>
-      <S.Section>
-        <S.Input
-          type="password"
-          placeholder="비밀번호를 입력해주세요!"
-          {...getTestInputProps('password')}
-          error={touched.password && errors.password}
-        />
-        {touched.password && errors.password && (
-          <S.ErrorMsg>{errors.password}</S.ErrorMsg>
-        )}
-      </S.Section>
-      <S.Section>
-        <S.Input
-          type="password"
-          placeholder="비밀번호를 다시 입력해주세요!"
-          {...getTestInputProps('passwordCheck')}
-          error={touched.passwordCheck && errors.passwordCheck}
-        />
-        {touched.passwordCheck && errors.passwordCheck && (
-          <S.ErrorMsg>{errors.passwordCheck}</S.ErrorMsg>
-        )}
-      </S.Section>
-      <S.SignupBtn
-        type="submit"
-        onClick={handlePressSignup}
-        disabled={
-          (touched.email && errors.email) ||
-          (touched.password && errors.password) ||
-          (touched.passwordCheck && errors.passwordCheck)
-        }
-      >
+      <S.SignupBtn type="submit" disabled={!isFormValid()}>
         제출
       </S.SignupBtn>
     </S.Form>
