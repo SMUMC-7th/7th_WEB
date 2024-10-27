49713a030995f9533a9779cf9393e1a4119e0fdf
Whatdoyumin <lym4963@naver.com>
2024-10-27 19:46:56 +0900

    ✨feat: 회원가입 유효성 검사 추가

--- 윰_이유민/5주차 미션/미션 1/src/utils/validate.js
+++ 윰_이유민/5주차 미션/미션 1/src/utils/validate.js
@@ -2,17 +2,22 @@ const emailPattern = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-]+/;
 
 // values: {email: , password: }
 function validateUser(values) {
-  const errors = {
-    email: '',
-    password: '',
-  };
+  const errors = {};
 
-  if (emailPattern.test(values.email) === false) {
+  // 이메일 유효성 검사
+  if (!values.email) {
+    errors.email = '이메일을 반드시 입력해주세요.';
+  } else if (emailPattern.test(values.email) === false) {
     errors.email = '올바른 이메일 형식이 아닙니다. 다시 확인해주세요!';
   }
 
-  if (values.password.length < 8 || values.password.length > 16) {
-    errors.password = '비밀번호는 8 ~ 16자 사이로 입력해주세요!';
+  // 비밀번호 유효성 검사
+  if (!values.password) {
+    errors.password = '비밀번호는 필수 입력 요소입니다.';
+  } else if (values.password.length < 8) {
+    errors.password = '비밀번호는 8자 이상으로 입력해주세요!';
+  } else if (values.password.length > 16) {
+    errors.password = '비밀번호는 16자 이하로 입력해주세요!';
   }
 
   return errors;
@@ -22,4 +27,17 @@ function validateLogin(values) {
   return validateUser(values);
 }
 
-export { validateLogin };
+function validateSignup(values) {
+  const errors = validateUser(values);
+
+  // 비밀번호 확인 유효성 검사
+  if (!values.passwordCheck) {
+    errors.passwordCheck = '비밀번호 검증 또한 필수 입력 요소입니다.';
+  } else if (values.passwordCheck !== values.password) {
+    errors.passwordCheck = '비밀번호가 일치하지 않습니다.';
+  }
+
+  return errors;
+}
+
+export { validateLogin, validateSignup };
