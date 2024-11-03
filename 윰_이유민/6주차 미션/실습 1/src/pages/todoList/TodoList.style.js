f6969f44ac640145806fcf669c8c29a2ca5d4011
Whatdoyumin <lym4963@naver.com>
2024-11-03 22:20:14 +0900

    💄 style: TodoList 컴포넌트 분리에 따른 스타일링 분리

--- 윰_이유민/6주차 미션/실습 1/src/pages/todoList/TodoList.style.js
+++ 윰_이유민/6주차 미션/실습 1/src/pages/todoList/TodoList.style.js
@@ -21,10 +21,6 @@ const SubmitForm = styled.form`
   flex-direction: row;
   justify-content: center;
   gap: 20px;
-
-  input {
-    width: 80%;
-  }
 `;
 
 const TodoListContainer = styled.div`
@@ -32,64 +28,6 @@ const TodoListContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 14px;
-  margin-top: 10px;
 `;
 
-const TodoItem = styled.div`
-  width: 80%;
-  display: flex;
-  gap: 20px;
-`;
-
-const Todo = styled.div`
-  width: 100%;
-  display: flex;
-  gap: 14px;
-`;
-
-const TodoId = styled.p`
-  width: 40px;
-`;
-
-const BtnContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-
-  > button {
-    width: 80px;
-    height: 30px;
-  }
-`;
-
-const DeleteButton = styled.button`
-  background-color: white;
-  color: #e84057;
-
-  &:hover {
-    background-color: #e84057;
-    color: white;
-  }
-`;
-
-const UpdateButton = styled.button`
-  color: #484848;
-
-  &:hover {
-    background-color: #484848;
-    color: white;
-  }
-`;
-
-export {
-  Container,
-  Header,
-  SubmitForm,
-  TodoListContainer,
-  TodoItem,
-  Todo,
-  TodoId,
-  BtnContainer,
-  DeleteButton,
-  UpdateButton,
-};
+export { Container, Header, SubmitForm, TodoListContainer };
