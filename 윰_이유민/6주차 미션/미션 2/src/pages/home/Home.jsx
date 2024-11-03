7df97027b87ae16b61bd8576df4a5712a1c3c6d8
Whatdoyumin <lym4963@naver.com>
2024-11-04 06:57:08 +0900

    ✨ feat: 영화 리스트 컴포넌트 분리에 따른 코드 수정

--- 윰_이유민/6주차 미션/미션 2/src/pages/home/Home.jsx
+++ 윰_이유민/6주차 미션/미션 2/src/pages/home/Home.jsx
@@ -1,28 +1,24 @@
-
-
-import * as S from "../movieList/MovieList.style";
-import MovieCard from "../../components/movieCard/MovieCard";
-import useCustomFetch from "../../hooks/useCustomFetch";
-import LoadingSpinner from "../../components/loadingSpinner/loadingSpinner";
+import MovieCard from '../../components/movieCard/MovieCard';
+import useCustomFetch from '../../hooks/useCustomFetch';
+import LoadingSpinner from '../../components/loadingSpinner/loadingSpinner';
+import { MovieList } from '../../components/movieList/MovieList';
 
 const HomePage = () => {
-  const {data: movies, isLoading, isError} = useCustomFetch(`/movie/popular?language=ko-kr`);
+  const {
+    data: movies,
+    isLoading,
+    isError,
+  } = useCustomFetch(`/movie/popular?language=ko-kr`);
 
   if (isLoading) {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
   }
 
   if (isError) {
-    return <div>에러가 발생했어요!</div>
+    return <div>에러가 발생했어요!</div>;
   }
 
-  return (
-    <S.Container>
-      {movies.data?.results.map((movie, _) => {
-        return <MovieCard key={movie.id} movie={movie} />;
-      })}
-    </S.Container>
-  );
+  return <MovieList movies={movies} />;
 };
 
 export default HomePage;
