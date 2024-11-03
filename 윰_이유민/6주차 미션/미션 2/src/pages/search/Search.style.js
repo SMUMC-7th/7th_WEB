ec0c2478ee3690ae9118ee64f173bb74c4f1d700
Whatdoyumin <lym4963@naver.com>
2024-11-04 06:58:04 +0900

    💄 style: 영화 검색 스타일링 적용

---
+++ 윰_이유민/6주차 미션/미션 2/src/pages/search/Search.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+  margin: 20px 30px;
+`;
+
+const ErrorBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export { Container, ErrorBox };
