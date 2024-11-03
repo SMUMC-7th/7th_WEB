9e1a02e2d029c2becf0f5df76362a8690aff6bab
Whatdoyumin <lym4963@naver.com>
2024-11-03 22:20:58 +0900

    💄 style: Input 스타일링 적용

--- 윰_이유민/6주차 미션/실습 1/src/components/input/Input.jsx
+++ 윰_이유민/6주차 미션/실습 1/src/components/input/Input.jsx
@@ -1,5 +1,7 @@
+import * as S from './Input.style';
+
 function Input({ value, onChange, defaultValue, type = 'text' }) {
-  return <input type={type} value={value} defaultValue={defaultValue} onChange={onChange} />;
+  return <S.Input type={type} value={value} defaultValue={defaultValue} onChange={onChange} />;
 }
 
 export default Input;
