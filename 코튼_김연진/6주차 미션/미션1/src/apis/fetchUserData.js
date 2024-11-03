a068203fa91c7386ecb24f080e2fbc80c14ca4c7
김연진 <kyj0719@gmail.com>
2024-11-04 01:21:42 +0900

    FIX: 문서보고 post 형식 수정했으나 여전히 POST 500 (Internal Server Error) 상태

--- 코튼_김연진/6주차 미션/미션1/src/apis/fetchUserData.js
+++ 코튼_김연진/6주차 미션/미션1/src/apis/fetchUserData.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3000';
 
+let isRedirecting = false;
+
 const fetchUserData = async () => {
     try {
         const accessToken = localStorage.getItem('accessToken');
@@ -17,6 +19,7 @@ const fetchUserData = async () => {
             console.warn(
                 'Access token이 만료 되었습니다. 재발급 시도중입니다.',
             );
+
             return await handleTokenRefreshAndRetry();
         } else {
             console.error('Error fetching user data:', error);
@@ -28,28 +31,36 @@ const fetchUserData = async () => {
 const handleTokenRefreshAndRetry = async () => {
     try {
         const refreshToken = localStorage.getItem('refreshToken');
-        // const id = localStorage.getItem('id');
+        console.log(refreshToken);
         const refreshResponse = await axios.post(
-            `${API_BASE_URL}/auth/token/access`,
+            `http://localhost:3000/auth/token/access`,
             {
-                refreshToken,
+                headers: {
+                    Authorization: `Bearer ${refreshToken}`,
+                },
             },
         );
-        console.log(refreshResponse);
-        console.log(refreshResponse.data);
+
         const { accessToken: newAccessToken } = refreshResponse.data;
         localStorage.setItem('accessToken', newAccessToken);
+
         const retryResponse = await axios.get(`${API_BASE_URL}/user/me`, {
             headers: {
                 Authorization: `Bearer ${newAccessToken}`,
             },
         });
-        console.log(retryResponse.data);
+
         return retryResponse.data;
     } catch (refreshError) {
         console.error('Token refresh failed:', refreshError);
-        window.location.href('/login');
+        redirectToLogin();
         return null;
+    }
+};
+const redirectToLogin = () => {
+    if (!isRedirecting) {
+        isRedirecting = true;
+        window.location.href('/login');
     }
 };
 
