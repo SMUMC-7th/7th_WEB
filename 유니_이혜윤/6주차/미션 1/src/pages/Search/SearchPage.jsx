91e3720d2568febaae855d3dd3ef8c2e2c967356
hyeyoon23 <51lhy@naver.com>
2024-11-06 16:54:28 +0900

    feat: 미션 2 검색 기능, debounce

--- 유니_이혜윤/6주차/미션 1/src/pages/Search/SearchPage.jsx
+++ 유니_이혜윤/6주차/미션 1/src/pages/Search/SearchPage.jsx
@@ -1,11 +1,64 @@
+import { useCallback, useState } from "react";
 import * as S from "./SearchPage.style";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import SearchMovieList from "@/components/SearchMovie/search-movie-list.jsx";
+
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+}
 
 const SearchPage = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams({
+    mq: "",
+  });
+  const mq = searchParams.get("mq");
+  console.log(mq);
+  //console.log(searchValue);
+
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      if (mq === value) return;
+      navigate(`/search?mq=${value}`);
+    }, 500),
+    [navigate, mq]
+  );
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
+
+  const handleSearchMovie = () => {
+    if (mq === searchValue) return;
+    navigate(`/search?mq=${searchValue}`);
+  };
+
+  const handleSearchMovieWithKeyboard = (e) => {
+    if (e.key === "Enter") {
+      handleSearchMovie();
+    }
+  };
+
   return (
-    <S.SearchContainer>
-      <input type="text" placeholder="영화 제목을 입력해주세요" />
-      <button type="submit">검색</button>
-    </S.SearchContainer>
+    <>
+      <S.SearchContainer>
+        <input
+          type="text"
+          placeholder="영화 제목을 입력해주세요"
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          onKeyDown={handleSearchMovieWithKeyboard}
+        />
+        <button onClick={handleSearchMovie}>검색</button>
+      </S.SearchContainer>
+      <SearchMovieList />
+    </>
   );
 };
 
--- 유니_이혜윤/6주차/미션 1/src/pages/Search/SearchPage.style.js
+++ 유니_이혜윤/6주차/미션 1/src/pages/Search/SearchPage.style.js
@@ -2,30 +2,46 @@ import styled from "styled-components";
 
 const SearchContainer = styled.div`
   width: 100%;
-  height: 100vh;
   margin: 20px 0;
 
   display: flex;
   justify-content: center;
 
   input {
+    flex: 1;
     width: 90%;
     height: 40px;
-    margin-left: 10px;
+    margin-left: 20px;
     padding: 0 10px;
-    border-radius: 5px;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
     border: none;
     outline: none;
   }
 
   button {
-    width: 5%;
+    width: 70px;
     height: 40px;
-    border-radius: 5px;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
     border: none;
     background-color: #f2085a;
     color: white;
+    margin-right: 20px;
   }
 `;
 
-export { SearchContainer };
+const MovieGridContainer = styled.div`
+  margin: 0 30px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(190px, 1fr));
+  gap: 20px;
+`;
+
+const NoSearch = styled.div`
+  text-align: center;
+  margin-top: 30px;
+  color: white;
+`;
+
+export { SearchContainer, MovieGridContainer, NoSearch };
