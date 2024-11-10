7617f8997fd4fb9e8fd0e59ab821513e9a1f3823
Whatdoyumin <lym4963@naver.com>
2024-11-11 01:56:57 +0900

    ✨ feat: accessToken과 refreshToken 상태 관리 추가

--- 윰_이유민/7주차 미션/미션 1/src/context/LoginContext.tsx
+++ 윰_이유민/7주차 미션/미션 1/src/context/LoginContext.tsx
@@ -1,14 +1,22 @@
 import { createContext, useState, ReactNode } from 'react';
 
-interface LoginContextTyoe {
+interface LoginContextType {
   isLogin: boolean;
   setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+  accessToken: string;
+  setAccessToken: React.Dispatch<React.SetStateAction<string>>;
+  refreshToken: string;
+  setRefreshToken: React.Dispatch<React.SetStateAction<string>>;
 }
 
 // 데이터 담고 있음
-export const LoginContext = createContext<LoginContextTyoe>({
+export const LoginContext = createContext<LoginContextType>({
   isLogin: false,
   setIsLogin: () => {},
+  accessToken: '',
+  setAccessToken: () => {},
+  refreshToken: '',
+  setRefreshToken: () => {},
 });
 
 interface LoginContextProviderProps {
@@ -17,12 +25,24 @@ interface LoginContextProviderProps {
 
 // 데이터를 감싸는 우산을 만듦
 export function LoginContextProvider({ children }: LoginContextProviderProps) {
-  const accessToken = localStorage.getItem('accessToken') || null;
+  const storedAccessToken = localStorage.getItem('accessToken') || '';
+  const storedRefreshToken = localStorage.getItem('refreshToken') || '';
 
-  const [isLogin, setIsLogin] = useState(accessToken === null ? false : true);
+  const [isLogin, setIsLogin] = useState(storedAccessToken !== '');
+  const [accessToken, setAccessToken] = useState(storedAccessToken);
+  const [refreshToken, setRefreshToken] = useState(storedRefreshToken);
 
   return (
-    <LoginContext.Provider value={{ setIsLogin, isLogin }}>
+    <LoginContext.Provider
+      value={{
+        setIsLogin,
+        isLogin,
+        accessToken,
+        setAccessToken,
+        refreshToken,
+        setRefreshToken,
+      }}
+    >
       {children}
     </LoginContext.Provider>
   );
