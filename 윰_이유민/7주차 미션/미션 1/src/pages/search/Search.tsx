f391bd6e66ce2fe73c8ea33d432c50b34b45b189
Whatdoyumin <lym4963@naver.com>
2024-11-11 04:00:48 +0900

    ✨ feat: 영화 검색 로딩 시 skeleton ui 적용 및 error 처리

--- 윰_이유민/7주차 미션/미션 1/src/pages/search/Search.tsx
+++ 윰_이유민/7주차 미션/미션 1/src/pages/search/Search.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'react-router-dom';
 import { MovieList } from '../../components/movieList/MovieList';
 import { axiosInstance } from '../../apis/axios-instance';
 import { useQuery } from '@tanstack/react-query';
+import { CardSkeletonList } from '../../components/movieCard/skeleton/CardSkeletonList';
 
 interface Movie {
   id: number;
@@ -22,12 +23,6 @@ const Search = () => {
 
   const url = `/search/movie?query=${mq}&include_adult=false&language=ko-KR&page=1`;
 
-  const errorBox = () => (
-    <S.ErrorBox>
-      <h3>검색어 {mq}에</h3>
-      <h3>해당하는 데이터가 없습니다.</h3>
-    </S.ErrorBox>
-  );
   const getMovieSearchData = async () => {
     const { data } = await axiosInstance.get(url);
     return data;
@@ -48,13 +43,16 @@ const Search = () => {
   return (
     <S.Container>
       <SearchBar params={mq} />
-      {mq ? (
-        movies?.results?.length ? (
-          <MovieList movies={movies} />
-        ) : (
-          errorBox()
-        )
-      ) : null}
+      {isLoading ? (
+        <CardSkeletonList number={20} />
+      ) : isError || movies?.results.length === 0 ? (
+        <S.ErrorBox>
+          <h3>검색어 "{mq}"에</h3>
+          <h3>해당하는 데이터가 없습니다.</h3>
+        </S.ErrorBox>
+      ) : (
+        mq && movies?.results.length && <MovieList movies={movies} />
+      )}
     </S.Container>
   );
 };
