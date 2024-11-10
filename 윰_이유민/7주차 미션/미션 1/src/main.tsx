60f18701dc6c8de01dbe4635f8c0359f4f4159e7
Whatdoyumin <lym4963@naver.com>
2024-11-10 22:08:54 +0900

    ✨ feat: useQuery 적용

--- 윰_이유민/7주차 미션/미션 1/src/main.tsx
+++ 윰_이유민/7주차 미션/미션 1/src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { LoginContextProvider } from './context/LoginContext.js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
-    <StrictMode>
-      <LoginContextProvider>
-        <App />
-      </LoginContextProvider>
-    </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <StrictMode>
+        <LoginContextProvider>
+          <App />
+        </LoginContextProvider>
+      </StrictMode>
+    </QueryClientProvider>
   );
 }
