c13183e320a29743a2b6dc9be22afe9a4061171f
Whatdoyumin <lym4963@naver.com>
2024-11-18 23:53:23 +0900

    ✨ feat: todo 데이터 get을 위한 custom hook 생성

---
+++ 윰_이유민/8주차 미션/미션 1/src/hooks/useFetchTodo.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { axiosInstance } from '../apis/axios-instance';
+
+const useFetchTodo = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axiosInstance.get(url);
+        setData(response.data);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+};
+
+export default useFetchTodo;
