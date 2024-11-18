02eda7bc46351ab8c47791f1485681dcbf1f140e
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2024-11-18 11:39:50 +0900

    FEAT: 카테고리 텍스트 꾸미기

--- 제리_문재영/7주차 미션/미션1/src/components/CategoryCard/CategoryCard.styled.js
+++ 제리_문재영/7주차 미션/미션1/src/components/CategoryCard/CategoryCard.styled.js
@@ -13,6 +13,11 @@ const Text = styled.div`
     color: ${props => props.color || 'white'};
     z-index: 1;
     position: absolute;
+    background-color: rgb(100,100,100);
+    border-radius: 5px;
+    top: 5px;
+    left: 5px;
+    padding: 3px;
 `
 const ImgBox = styled.img`
     width: 350px;
