b1600deecfc9325c1e8ba3d329b2544dae150936
Whatdoyumin <lym4963@naver.com>
2024-11-19 05:59:43 +0900

    ✨ feat: 다른 method도 사용할 수 있도록 변경

--- 윰_이유민/8주차 미션/미션 1/src/hooks/useFetchTodo.js
+++ 윰_이유민/8주차 미션/미션 1/src/hooks/useFetchTodo.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import { axiosInstance } from '../apis/axios-instance';
 
-const usePostTodo = (url) => {
+const useFetchTodo = (url, method = 'post') => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const postTodo = async (todoData) => {
+  const requestTodo = async (todoData) => {
     setIsLoading(true);
     try {
-      const response = await axiosInstance.post(url, todoData);
+      const response = await axiosInstance[method](url, todoData);
       return response.data;
     } catch (error) {
       setIsError(true);
@@ -18,7 +18,7 @@ const usePostTodo = (url) => {
     }
   };
 
-  return { postTodo, isLoading, isError };
+  return { requestTodo, isLoading, isError };
 };
 
-export default usePostTodo;
+export default useFetchTodo;
