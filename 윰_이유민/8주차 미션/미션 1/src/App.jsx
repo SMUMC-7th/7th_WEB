55cb610cb654f8d96978b601472db0224cdc48d4
Whatdoyumin <lym4963@naver.com>
2024-11-18 23:48:08 +0900

    ✨ feat: 라우터 설정

--- 윰_이유민/8주차 미션/미션 1/src/App.jsx
+++ 윰_이유민/8주차 미션/미션 1/src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import TodoList from './pages/todoList/TodoList';
+import TodoDetails from './pages/todoDetail/TodoDetails';
+import { RootLayout } from './layout/root-layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <TodoList />,
+      },
+      {
+        path: '/todo/:todoId',
+        element: <TodoDetails />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
