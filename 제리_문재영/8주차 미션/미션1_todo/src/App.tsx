d987897ea01af69fda3bd1451648925fc4f643d5
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2024-11-25 15:37:48 +0900

    FEAT: todo 초기세팅

--- 제리_문재영/8주차 미션/미션1_todo/src/App.tsx
+++ 제리_문재영/8주차 미션/미션1_todo/src/App.tsx
@@ -1,16 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './output.css';
 import './App.css'
+import styled from 'styled-components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, content);
+    
+  }
   return (
     <>
-      야호
+    <form
+    className='flex flex-col'
+    onSubmit={handleSubmit}
+    >
+      <input 
+      className='p-2.5 border border-purple-500 rounded-lg'
+      name='title'
+      placeholder='제목을 입력해주세요..'
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      />
+      <input 
+      className='p-2.5 border border-purple-500 rounded-lg'
+      name='content'
+      placeholder='내용을 입력해주세요..'
+      value={content} 
+      onChange={(e) => setContent(e.target.value)}
+      />
+      <button 
+      className='bg-black'
+      type='submit'>투두 생성</button>
+
+    </form>
+    
     </>
   )
 }
 
 export default App
+
+// const Button = styled.button`
+//   padding: 10px;
+//   border: 1px solid black;
+// `
--- 제리_문재영/8주차 미션/미션1_todo/tailwind.config.js
+++ 제리_문재영/8주차 미션/미션1_todo/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+
+const px0_200 = Array.from({length:201}, (_,idx) => `${idx}px`);
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], //tailwindcss의 영향범위다 src내에 어떤 폴더안에 있는 js등등 다
   theme: {
-    extend: {},
+    extend: {
+      fontSize: px0_200,
+    },
   },
   plugins: [],
 }
+
