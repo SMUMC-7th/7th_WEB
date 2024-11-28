fe14c3a912eda7b0f1b416d1dff9ab841812b658
Whatdoyumin <lym4963@naver.com>
2024-11-28 15:35:26 +0900

    ✨ feat: 로그인 여부 전역 상태 관리

--- 윰_이유민/최종 미션/src/main.tsx
+++ 윰_이유민/최종 미션/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { LoginContextProvider } from './context/LoginContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginContextProvider>
+      <App />
+    </LoginContextProvider>
   </StrictMode>,
 );
