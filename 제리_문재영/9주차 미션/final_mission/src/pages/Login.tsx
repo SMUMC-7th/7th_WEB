69cce4846ca1bccaaf03763709411c8ea48e4a7b
jaeyoungmun <mjy0694@naver.com>
2024-12-02 22:02:12 +0900

    FEAT: 로그인 토큰 관리실패

--- 제리_문재영/9주차 미션/final_mission/src/pages/Login.tsx
+++ 제리_문재영/9주차 미션/final_mission/src/pages/Login.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import useLoginMutation from "../hooks/auth/useLoginMutation";
-
+import Cookies from "js-cookie";
 export const Login = () => {
   const [id, setId] = useState("");
   const [pwd, setPwd] = useState("");
 
   const { mutate } = useLoginMutation();
 
+  // console.log(mutate);
+
   return (
     <>
-      <form className="login">
+      <form onSubmit={(e) => e.preventDefault()}>
         <input
+          className="border border-black"
+          placeholder="아이디를 입력해주세요..."
           name="id"
           value={id}
           onChange={(e) => {
@@ -18,6 +22,8 @@ export const Login = () => {
           }}
         />
         <input
+          className="border border-black"
+          placeholder="비밀번호를 입력해주세요..."
           name="password"
           value={pwd}
           onChange={(e) => {
@@ -25,11 +31,15 @@ export const Login = () => {
           }}
         />
         <button
+          type="submit"
           onClick={() => {
             mutate(
               { email: id, password: pwd },
               {
                 onSuccess: () => {
+                  let token = localStorage.getItem("accessToken");
+                  console.log(token); //null값..ㅠ
+
                   alert("로그인 성공");
                 },
                 onError: (error) => {
