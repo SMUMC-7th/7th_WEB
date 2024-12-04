422ccdabdb9b917ddefc346caa5f8e018ce02631
Whatdoyumin <lym4963@naver.com>
2024-12-04 18:50:45 +0900

    ✨ feat: 게시글 정렬 nav 구현

--- 윰_이유민/최종 미션/src/components/PostsNav.tsx
+++ 윰_이유민/최종 미션/src/components/PostsNav.tsx
@@ -2,20 +2,44 @@ import { Link } from 'react-router-dom';
 import { IoMdTrendingUp } from 'react-icons/io';
 import { IoMdTime } from 'react-icons/io';
 import { IoHeartCircle } from 'react-icons/io5';
+import { TPostNav } from '../type/post';
 
-const PostsNav = () => {
+const PostsNav = ({
+  isSelected,
+  setIsSelected,
+}: {
+  isSelected: TPostNav;
+  setIsSelected: React.Dispatch<React.SetStateAction<TPostNav>>;
+}) => {
   return (
     <div className='w-full h-16 flex justify-between items-center'>
       <nav className='flex gap-10 text-xl items-center'>
-        <div className='flex gap-2 items-center cursor-pointer'>
+        <div
+          className={`flex gap-2 items-center cursor-pointer ${
+            isSelected === 'now_trending'
+              ? 'border-b border-gray-800 text-gray-800'
+              : 'text-gray-400'
+          }`}
+          onClick={() => setIsSelected('now_trending')}
+        >
           <IoMdTrendingUp />
           <p>지금 뜨는</p>
         </div>
-        <div className='flex gap-2 items-center cursor-pointer'>
+        <div
+          className={`flex gap-2 items-center cursor-pointer ${
+            isSelected === 'recent' ? 'border-b border-gray-800 text-gray-800' : 'text-gray-400'
+          }`}
+          onClick={() => setIsSelected('recent')}
+        >
           <IoMdTime />
           <p>최신</p>
         </div>
-        <div className='flex gap-2 items-center cursor-pointer'>
+        <div
+          className={`flex gap-2 items-center cursor-pointer ${
+            isSelected === 'popular' ? 'border-b border-gray-800 text-gray-800' : 'text-gray-400'
+          }`}
+          onClick={() => setIsSelected('popular')}
+        >
           <IoHeartCircle />
           <p>인기</p>
         </div>
